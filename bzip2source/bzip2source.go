b3d63eb7fb476657c3d3101295b11937eed05520
jesus <jesus@itch.io>
2017-12-15 13:02:21 +0100

    bzip2source is in!

---
+++ bzip2source/bzip2source.go
@@ -0,0 +1,142 @@
+package bzip2source
+
+import (
+	"fmt"
+
+	"github.com/go-errors/errors"
+	"github.com/itchio/kompress/bzip2"
+	"github.com/itchio/savior"
+	"github.com/mohae/deepcopy"
+)
+
+type bzip2Source struct {
+	// input
+	source savior.Source
+
+	// params
+	threshold int64
+
+	// internal
+	sr      bzip2.SaverReader
+	offset  int64
+	counter int64
+
+	checkpoint *bzip2sourceCheckpoint
+}
+
+type bzip2sourceCheckpoint struct {
+	Offset            int64
+	SourceCheckpoint  *savior.SourceCheckpoint
+	Bzip2Checkpoint   *bzip2.Checkpoint
+}
+
+var _ savior.Source = (*bzip2Source)(nil)
+
+func New(source savior.Source, threshold int64) *bzip2Source {
+	return &bzip2Source{
+		source:    source,
+		threshold: threshold,
+	}
+}
+
+func (bs *bzip2Source) Save() (*savior.SourceCheckpoint, error) {
+	if bs.checkpoint != nil {
+		c := &savior.SourceCheckpoint{
+			Offset: bs.offset,
+			Data:   bs.checkpoint,
+		}
+		return c, nil
+	}
+	return nil, nil
+}
+
+func (bs *bzip2Source) Resume(checkpoint *savior.SourceCheckpoint) (int64, error) {
+	bs.counter = 0
+	bs.checkpoint = nil
+
+	if checkpoint != nil {
+		if ourCheckpoint, ok := checkpoint.Data.(*bzip2sourceCheckpoint); ok {
+			sourceOffset, err := bs.source.Resume(ourCheckpoint.SourceCheckpoint)
+			if err != nil {
+				return 0, errors.Wrap(err, 0)
+			}
+
+			bc := ourCheckpoint.Bzip2Checkpoint
+			if sourceOffset == bc.Roffset {
+				bs.sr, err = bc.Resume(bs.source)
+				if err != nil {
+					savior.Debugf(`bzip2source: could not use bzip2 checkpoint at R=%d`, bc.Roffset)
+					// well, let's start over
+					_, err = bs.source.Resume(nil)
+					if err != nil {
+						return 0, errors.Wrap(err, 0)
+					}
+				} else {
+					bs.offset = ourCheckpoint.Offset
+					return bs.offset, nil
+				}
+			} else {
+				savior.Debugf(`bzip2source: expected source to resume at %d but got %d`, bc.Roffset, sourceOffset)
+			}
+		}
+	}
+
+	// start from beginning
+	sourceOffset, err := bs.source.Resume(nil)
+	if err != nil {
+		return 0, errors.Wrap(err, 0)
+	}
+
+	if sourceOffset != 0 {
+		msg := fmt.Sprintf("bzip2source: expected source to resume at start but got %d", sourceOffset)
+		return 0, errors.New(msg)
+	}
+
+	bs.sr = bzip2.NewSaverReader(bs.source)
+
+	bs.offset = 0
+	return 0, nil
+}
+
+func (bs *bzip2Source) Read(buf []byte) (int, error) {
+	n, err := bs.sr.Read(buf)
+	bs.offset += int64(n)
+	bs.counter += int64(n)
+	if bs.counter > bs.threshold {
+		bs.sr.WantSave()
+		bs.counter = 0
+	}
+
+	if err != nil {
+		if err == bzip2.ReadyToSaveError {
+			bzip2Checkpoint, saveErr := bs.sr.Save()
+			if saveErr != nil {
+				return n, saveErr
+			}
+
+			sourceCheckpoint, sourceErr := bs.source.Save()
+			if saveErr != nil {
+				return n, sourceErr
+			}
+
+			savior.Debugf("bzip2source: saving, bzip2 rOffset = %d, sourceCheckpoint.Offset = %d", bzip2Checkpoint.Roffset, sourceCheckpoint.Offset)
+
+			bs.checkpoint = &bzip2sourceCheckpoint{
+				Offset:            bs.offset,
+				Bzip2Checkpoint:   deepcopy.Copy(bzip2Checkpoint).(*bzip2.Checkpoint),
+				SourceCheckpoint:  sourceCheckpoint,
+			}
+
+			savior.Debugf("bzip2source: saved checkpoint at byte %d", bs.offset)
+			err = nil
+		}
+	}
+
+	return n, err
+}
+
+func (bs *bzip2Source) ReadByte() (byte, error) {
+	buf := []byte{0}
+	_, err := bs.Read(buf)
+	return buf[0], err
+}
---
+++ bzip2source/bzip2source_test.go
@@ -0,0 +1,52 @@
+package bzip2source_test
+
+import (
+	"bytes"
+	"log"
+	"testing"
+
+	"github.com/go-errors/errors"
+	"github.com/itchio/savior"
+
+	"github.com/dsnet/compress/bzip2"
+
+	humanize "github.com/dustin/go-humanize"
+	"github.com/itchio/savior/bzip2source"
+	"github.com/itchio/savior/seeksource"
+	"github.com/itchio/savior/semirandom"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBzip2Source(t *testing.T) {
+	reference := semirandom.Generate(4 * 1024 * 1024 /* 4 MiB of random data */)
+	compressed, err := bzip2Compress(reference)
+	assert.NoError(t, err)
+
+	log.Printf("uncompressed size: %s", humanize.IBytes(uint64(len(reference))))
+	log.Printf("  compressed size: %s", humanize.IBytes(uint64(len(compressed))))
+
+	source := seeksource.New(bytes.NewReader(compressed))
+	bs := bzip2source.New(source, 256*1024 /* 128 KiB threshold */)
+
+	savior.RunSourceTest(t, bs, reference)
+}
+
+func bzip2Compress(input []byte) ([]byte, error) {
+	compressedBuf := new(bytes.Buffer)
+	w, err := bzip2.NewWriter(compressedBuf, &bzip2.WriterConfig{Level: 2})
+	if err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+
+	_, err = w.Write(input)
+	if err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+
+	err = w.Close()
+	if err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+
+	return compressedBuf.Bytes(), nil
+}
